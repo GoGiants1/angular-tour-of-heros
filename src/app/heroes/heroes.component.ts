091273df856749ac547b17bea139ebe0ed022fec
GoGiants1 <chooi221@snu.ac.kr>
2022-03-10 17:05:25 +0900

    히어로 목록 표시 - 목록 표시할 때, *ngFor 사용 - undefined 걸러낼 때, *ngIf 사용 - class binding 사용하면 css 스타일 클래스를 조건부로 적용 가능하다

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = { id: 1, name: 'Windstorm' };
+  heroes = HEROES;
 
   constructor() {}
 
   // 컴포넌트 초기화 로직 ngOnInit에 작성할 것.
   ngOnInit(): void {}
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
