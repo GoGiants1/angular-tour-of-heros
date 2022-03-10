af132bb800b498259cb56a93456f029e0079a792
GoGiants1 <chooi221@snu.ac.kr>
2022-03-10 16:45:16 +0900

    히어로 에디터 구현 - Angular CLI를 사용해서 컴포넌트 생성 - 히어로 컴포넌트를 AppComponent 셀에 추가(ng generate 하면 자동으로 추가) - UppercasePipe를 이용해서 대문자로 이름 표시 - ngModel 사용해서, 양방향 데이터 바인딩 - AppModule 공부 - AppModule에 FormsModule 추가 후, ngModel 정상 작동 - 컴포넌트는 @NgModule에 반드시 추가되어야 함.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero: Hero = { id: 1, name: 'Windstorm' };
 
-  constructor() { }
+  constructor() {}
 
   // 컴포넌트 초기화 로직 ngOnInit에 작성할 것.
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
