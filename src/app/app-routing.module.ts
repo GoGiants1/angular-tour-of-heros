076f112ee2f7e23c48f1b95b1b5ba818ec989823
GoGiants1 <chooi221@snu.ac.kr>
2022-03-11 14:12:13 +0900

    Add routerOutlet

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+// ng generate module app-routing --flat --module=app 로 생성
+// --flat: 새로운 폴더를 만들지 않고 src/app 폴더에 파일 생성
+// --module=app: Angular CLI가 라우팅 모듈을 AppModule의 imports 배열에 추가
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  // 애플리케이션 최상위 계층에 존재하는 라우터를 설정할 때는 forRoot() 메소드를 사용합니다.
+  // forRoot() 메소드를 사용하면 라우팅과 관련된 서비스 프로바이더와 디렉티브를 애플리케이션에 제공할 수 있으며,
+  // 브라우저에서 변경되는 URL을 감지할 수 있습니다.
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -8,6 +8,7 @@ import { MessageService } from '../message.service';
 export class MessagesComponent implements OnInit {
   // messageService 프로퍼티는 템플릿에 바인딩되기 때문에,
   // 반드시 public으로 선언해야 합니다.
+  // Angular에서는 public 으로 선언된 컴포넌트 프로퍼티만 바인딩할 수 있음.
   constructor(public messageService: MessageService) {}
 
   ngOnInit(): void {}
