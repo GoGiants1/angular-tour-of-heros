211cd227d0d081dd0ed1fee03877bd140c1ad721
GoGiants1 <chooi221@snu.ac.kr>
2022-03-11 14:59:45 +0900

    Add http apis

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,8 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 // 의존성으로 주입 가능, 다른 의존성을 주입받을 수도 있음.
 @Injectable({
@@ -11,18 +13,53 @@ import { MessageService } from './message.service';
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private messageService: MessageService, private http: HttpClient) {}
+
+  private heroesUrl = 'api/heroes'; // 웹 API 형식의 URL로 사용
+
+  /** HeroService에서 보내는 메시지는 MessageService가 화면에 표시합니다. */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: 리모트 서버로 에러 메시지 보내기
+      console.error(error);
+
+      // TODO: 사용자가 이해할 수 있는 형태로 변환하기
+      this.log(`${operation} failed: ${error.message}`);
+
+      // 애플리케이션 로직이 끊기지 않도록 기본값으로 받은 객체를 반환합니다.
+      return of(result as T);
+    };
+  }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES); // await 같은 역할?
+    // const heroes = of(HEROES); // await 같은 역할?
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    // return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', [])),
+    );
   }
   getHero(id: number): Observable<Hero> {
-    // 지금은 히어로의 `id` 프로퍼티가 항상 존재한다고 간주.
-    // TODO: 에러 처리
-    const hero = HEROES.find((h) => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`)),
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`update hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero')),
+    );
   }
 }
